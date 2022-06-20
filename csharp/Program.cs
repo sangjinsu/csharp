361cbb304bcc3df27ea1519541b7669e9f83e78f
sangjinsu <climbplant39@kakao.com>
2022-06-20 22:44:17 +0900

    데이터 형식

---
+++ csharp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a;
+            a = true;
+            string b = "123456";
+            Console.WriteLine(b);
+        }
+    }
+}
