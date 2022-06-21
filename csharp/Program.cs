54b25132fc129b15d67e8c19fb2483dea89c88f4
sangjinsu <climbplant39@kakao.com>
2022-06-21 22:47:45 +0900

    흐름 제어

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -2,12 +2,34 @@
 {
     class Program
     {
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void AddOne(ref int a)
+        {
+            a = a + 1;
+        }
+
+        static void Divide(int a, int b, out int result1)
+        {
+            result1 = a / b;
+        }
+        
         static void Main(string[] args)
         {
-            bool a;
-            a = true;
-            string b = "123456";
+            const string b = "123456";
             Console.WriteLine(b);
+
+            if (true)
+            {
+                Console.WriteLine("hello tired day");
+            }
+
+            int result1;
+            Divide(10,3, out result1);
+            Console.WriteLine(result1);
         }
     }
 }
