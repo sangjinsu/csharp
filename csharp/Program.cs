4350e3f3dc9aea1b3a9cf6cbb3103907a8e5468e
sangjinsu <climbplant39@kakao.com>
2022-06-26 18:27:10 +0900

    객체지향 여행

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -7,29 +7,10 @@
             return a + b;
         }
 
-        static void AddOne(ref int a)
-        {
-            a = a + 1;
-        }
-
-        static void Divide(int a, int b, out int result1)
-        {
-            result1 = a / b;
-        }
-        
         static void Main(string[] args)
         {
-            const string b = "123456";
-            Console.WriteLine(b);
-
-            if (true)
-            {
-                Console.WriteLine("hello tired day");
-            }
-
-            int result1;
-            Divide(10,3, out result1);
-            Console.WriteLine(result1);
+            int ret = Add(5, 100);
+            Console.WriteLine(ret);
         }
     }
 }
